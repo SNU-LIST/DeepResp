a8b1d23630aa5bd56de95ea0ec1cbe90b4a646ea
Hongjun An <57519974+HongjunAn123@users.noreply.github.com>
2020-07-19 19:10:15 +0900

    Update evaluation_params.py

--- code/evaluation_params.py
+++ code/evaluation_params.py
@@ -5,5 +5,4 @@ datapath_name = '../data/evaluation/simulated/input' # path of evaluation datase
 resultpath_name = '../data/evaluation/simulated/prediction'# path for result
 
 # network paramter
-phase_normalized_value = 2 * 0.05 # (simulation paramter) const: scale of phase error to normalize (frequency shifht (Hz) * TE (sec))
 batch_size = 200 # batch size
