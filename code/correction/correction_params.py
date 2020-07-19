67a902aa8f0421eeebad365e732858c731a0e1ac
Hongjun An <57519974+HongjunAn123@users.noreply.github.com>
2020-07-19 18:11:51 +0900

    Create correction_params.py

---
+++ code/correction/correction_params.py
@@ -0,0 +1,9 @@
+img_path = '../../data/evaluation/simulated/input' # path of the corrupted image
+network_phase_path = '../../data/evaluation/simulated/prediction' # path of the network-genarated phase errors
+                                                           # For simulated data, '../data/evaluation/simulated/input'
+                                                           # For in-vivo data, '../data/evaluation/invivo/input'
+ref_phase_path = '../../data/evaluation/simulated/output' # path of the refrence phase errors
+result_path = '../../data/evaluation/simulated/corection'# path for results of the corrected image to save
+
+# network paramter
+phase_normalized_value = 2 * 0.05
